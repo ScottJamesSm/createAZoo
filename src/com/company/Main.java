1b965c57d517eabca1085a1057c8706238d8d38f
Scott James Smith <slocknog1982@gmail.com>
2018-06-08 11:06:12 -0400

    added menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("welcome to the zoo");
+        Menu menu = new Menu();
+        menu.mainMenu();
     }
 }
---
+++ src/com/company/Menu.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner input = new Scanner(System.in);
+
+    public void  mainMenu() {
+
+
+        System.out.println("Please Choose an option. " + "\n1. Manage People" + "\n2. Manage Animals" + "\n3. Manage Inventory");
+                }
+}
