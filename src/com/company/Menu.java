6f3a94df8ccee3da77a6151a9c9b1541a2fc7381
Scott James Smith <slocknog1982@gmail.com>
2018-06-08 11:41:30 -0400

    added more code to menu

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -8,5 +8,36 @@ public class Menu {
 
 
         System.out.println("Please Choose an option. " + "\n1. Manage People" + "\n2. Manage Animals" + "\n3. Manage Inventory");
-                }
+        //Handles user input for navigation
+        switch (input.nextInt()) {
+            case 1:
+                //manage People
+                managePeople();
+                break;
+            case 2:
+                //manage Animals
+                manageAnimals();
+                break;
+            case 3:
+                //manage Inventory
+                manageInventory();
+                break;
+            default:
+                //handle wrong number
+                // Restart method to allow user to try again
+    }
+
+    }
+
+    private void manageInventory() {
+        
+    }
+
+    private void manageAnimals() {
+
+    }
+
+    private void managePeople() {
+
+    }
 }
