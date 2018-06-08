c37cf6a51052d2562bedb8addb9850ea822e8c69
Scott James Smith <slocknog1982@gmail.com>
2018-06-08 12:39:49 -0400

    fixed error with more code

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,35 +1,50 @@
 package com.company;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Menu {
     private Scanner input = new Scanner(System.in);
 
-    public void  mainMenu() {
+    public void  mainMenu() throws InputMismatchException{
 
 
         System.out.println("Please Choose an option. " + "\n1. Manage People" + "\n2. Manage Animals" + "\n3. Manage Inventory");
         //Handles user input for navigation
-        switch (input.nextInt()) {
-            case 1:
-                //manage People
-                managePeople();
-                break;
-            case 2:
-                //manage Animals
-                manageAnimals();
-                break;
-            case 3:
-                //manage Inventory
-                manageInventory();
-                break;
-            default:
-                //handle wrong number
-                // Restart method to allow user to try again
-    }
-
+        try {
+            switch (input.nextInt()) {
+                case 1:
+                    //manage People
+                    managePeople();
+                    break;
+                case 2:
+                    //manage Animals
+                    manageAnimals();
+                    break;
+                case 3:
+                    //manage Inventory
+                    manageInventory();
+                    break;
+                default:
+                    //handle wrong number
+                    System.out.println("that is not a valid entry. Please enter a number between 1 and 3");
+                    mainMenu();
+                    // Restart method to allow user to try again
+            }
+        }catch(InputMismatchException ime){
+            // Handle if a  user puts in something that is not an int
+            input.nextLine();
+            System.out.println("that is not a valid entry please enter a number between 1 and 3");
+            mainMenu();
+        }
     }
     //Handles incentory options
     private void manageInventory() {
+        try {
+            // enter code here that you would like to run that MAY throw an exceptio
+
+        } catch (InputMismatchException ime) {
+            //what you want your program to do IF this exception happens
+        }
 
     }
     //handles Animals options
