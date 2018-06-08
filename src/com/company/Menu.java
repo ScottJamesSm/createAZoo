170b24715975b667e1e94e6e8e67e204b81b8f2a
Scott James Smith <slocknog1982@gmail.com>
2018-06-08 13:57:52 -0400

    added try to code

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -60,6 +60,7 @@ public class Menu {
     }
     // handles People options
     private void managePeople() {
+        try {
 
         //find out what type of people to manage
         System.out.println("are you" + "\n1. an Employee" + "\n2. a Visitor" + "\n3. go back" + "\n4. exit");
